2c4cf5e6d5a09a2c660570c2568e8f4d87199215
filipebsmaia <filipebsmaia@gmail.com>
2020-09-28 23:06:59 -0300

    feat: route validade params

--- criptomoeda/api/src/routes/cryptocurrency.routes.ts
+++ criptomoeda/api/src/routes/cryptocurrency.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate, Segments, Joi } from "celebrate";
 
 import CryptoCurrencyController from "@app/controllers/CryptoCurrencyController";
 import TopCryptoCurrencyController from "@app/controllers/TopCryptoCurrencyController";
@@ -13,8 +14,24 @@ const routes = Router();
 
 routes.use(cacheControl);
 
-routes.get("/", cryptoCurrencyController.index);
+routes.get(
+  "/",
+  celebrate({
+    [Segments.QUERY]: {
+      name: Joi.string().required(),
+    },
+  }),
+  cryptoCurrencyController.index
+);
 routes.get("/top", topCryptoCurrencyController.index);
-routes.get("/history/:symbol", priceHistoryCryptoCurrencyController.show);
+routes.get(
+  "/history/:symbol",
+  celebrate({
+    [Segments.PARAMS]: {
+      symbol: Joi.string().required(),
+    },
+  }),
+  priceHistoryCryptoCurrencyController.show
+);
 
 export default routes;
--- criptomoeda/api/src/routes/users.routes.ts
+++ criptomoeda/api/src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate, Segments, Joi } from "celebrate";
 import UserController from "@app/controllers/UserController";
 import SessionsController from "@app/controllers/SessionsController";
 
@@ -7,7 +8,17 @@ const sessionsController = new SessionsController();
 
 const routes = Router();
 
-routes.post("/", userController.create);
+routes.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create
+);
 
 routes.post("/sessions", sessionsController.create);
 
